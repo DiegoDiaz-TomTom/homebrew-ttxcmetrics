95edfb31cb00146283365deda5d2093de6dc85b9
DiegoDiaz-TomTom <diego.diaz@tomtom.com>
2024-12-04 15:58:33 +0100

    chore: remove bottle

--- Formula/ttxcmetrics.rb
+++ Formula/ttxcmetrics.rb
@@ -5,11 +5,6 @@ class Ttxcmetrics < Formula
   sha256 "2fd6f09890b6d1fec0bd869d54fef28cb95a9ad6ec399ccda87dc91ccd911ce7"
   license "Apache 2.0"
 
-  bottle do
-    sha256 cellar: :any, arm64_sequoia: "2fd6f09890b6d1fec0bd869d54fef28cb95a9ad6ec399ccda87dc91ccd911ce7"
-    sha256 cellar: :any, arm64_sonoma:  "2fd6f09890b6d1fec0bd869d54fef28cb95a9ad6ec399ccda87dc91ccd911ce7"
-  end
-
   def install
     bin.install "TTXCMetrics"
   end
