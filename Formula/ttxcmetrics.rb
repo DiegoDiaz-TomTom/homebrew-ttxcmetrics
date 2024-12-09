cf2379e1015f7164e316cc9b0fbd685169450ae6
DiegoDiaz-TomTom <diego.diaz@tomtom.com>
2024-12-09 11:39:15 +0100

    chore: bump release to v1.1.0

--- Formula/ttxcmetrics.rb
+++ Formula/ttxcmetrics.rb
@@ -1,8 +1,8 @@
 class Ttxcmetrics < Formula
   desc "TomTom's XCMetrics Client"
   homepage "https://github.com/tomtom-forks/XCMetrics/tree/tomtom-main"
-  url "https://github.com/tomtom-forks/XCMetrics/releases/download/v1.0.0/TTXCMetrics"
-  sha256 "2fd6f09890b6d1fec0bd869d54fef28cb95a9ad6ec399ccda87dc91ccd911ce7"
+  url "https://github.com/tomtom-forks/XCMetrics/releases/download/v1.1.0/TTXCMetrics"
+  sha256 "76dafdd835fee7f7f5dbe96e573d7845d92c4264c39b7362dfc3e9be72fb3efe"
   license "Apache 2.0"
 
   def install
